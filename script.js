92ecf9310660f9d92208209b4ccd1e31b6085aa4
Nauroz <naurozrashid31@gmail.com>
2021-08-21 04:16:01 +0600

    without discount

--- script.js
+++ script.js
@@ -1,4 +1,99 @@
+// totlaprice function
+function totalPrice(){
+    const bestPricetext=document.getElementById('best-price').innerText;
+    const bestPrice=parseFloat(bestPricetext);
+    const memoryCosttext=document.getElementById('addMemoryCost').innerText;
+    const memoryCost=parseFloat(memoryCosttext);
+    const StorageCosttext=document.getElementById('addStorageCost').innerText;
+    const StorageCost=parseFloat(StorageCosttext);
+    const deliveryCosttext=document.getElementById('addDeliveryCost').innerText;
+    const deliveryCost=parseFloat(deliveryCosttext);
+    const totalprice =memoryCost+StorageCost+deliveryCost+bestPrice;
+    return totalprice;
+}
 // memory add event handler
-document.getElementById('addMemory1').addEventListener('click',function(){
+// 16gb memory add
+function memoryCost(btnId,memoryCost){
+    const displayValue=document.getElementById(btnId);
+    displayValue.innerText=memoryCost;
+    const totalPriceid=document.getElementById('total-price');
+    totalPriceid.innerText=totalPrice();
+    document.getElementById('total-priceMain').innerText=totalPrice();
+}
+document.getElementById('addMemory2').addEventListener('click',function(){
+    
+    memoryCost('addMemoryCost',180);
+    
+    
     
 })
+// 8gb memory add
+document.getElementById('addMemory1').addEventListener('click',function(){
+    
+    memoryCost('addMemoryCost',0);
+    
+    
+    
+})
+// storage event handler
+// 256gb storage add
+function addStorage(storageId,storagePrice){
+    const displayValue=document.getElementById(storageId);
+    displayValue.innerText=storagePrice;
+    const totalPriceid=document.getElementById('total-price');
+    totalPriceid.innerText=totalPrice();
+    document.getElementById('total-priceMain').innerText=totalPrice();
+}
+document.getElementById('addStorage1').addEventListener('click',function(){
+    
+    addStorage('addStorageCost',0);
+    
+    
+})
+// 512gb storage add
+document.getElementById('addStorage2').addEventListener('click',function(){
+    
+    addStorage('addStorageCost',100);
+    
+    
+    
+})
+// 1TB storage add
+document.getElementById('addStorage3').addEventListener('click',function(){
+    addStorage('addStorageCost',180);
+    
+    
+})
+
+// delivery charge event handler
+// free delivery
+function deliveryCharge(deliveryid,deliveryCharge){
+    const displayValue=document.getElementById(deliveryid);
+    displayValue.innerText=deliveryCharge;
+    const totalPriceid=document.getElementById('total-price');
+    totalPriceid.innerText=totalPrice();
+    document.getElementById('total-priceMain').innerText=totalPrice();
+}
+document.getElementById('deliverybtn1').addEventListener('click',function(){
+    
+    deliveryCharge('addDeliveryCost',0);
+    
+    
+})
+document.getElementById('deliverybtn2').addEventListener('click',function(){
+    
+    deliveryCharge('addDeliveryCost',20);
+    
+})
+
+// apply button click
+document.getElementById('apply-btn').addEventListener('click',function(){
+
+})
+
+// total count function with discount
+
+function totalCount(){
+    const input=document.getElementById(discount-input);
+    const inputValue=input.value;
+}
